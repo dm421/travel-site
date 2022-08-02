acb55a1bcc7d65b65fb398093accd63d53271e72
dm421 <dmieskowski@gmail.com>
2022-08-02 13:23:00 -0400

    complete through section 19 (build process configuration)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const { ModuleFilenameHelpers } = require('webpack')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require('fs-extra')
+const { template } = require('lodash')
 
 const postCSSPlugins = [
     require('postcss-import'),
@@ -10,16 +17,48 @@ const postCSSPlugins = [
     require('autoprefixer')
 ]
 
-module.exports = {
+class RunAfterCompile {
+    apply(compiler) {
+        compiler.hooks.done.tap('Copy images', function() {
+            fse.copySync('./app/assets/images', './docs/assets/images')
+        })
+    }
+}
+
+let cssConfig = {
+    test: /\.css$/i,
+    use: ['css-loader', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]   // JavaScript loads css files
+}
+
+let pages = fse.readdirSync('./app').filter(function(file) {
+    return file.endsWith('.html')
+}).map(function(page) {
+    return new HtmlWebpackPlugin({
+        filename: page,
+        template: `./app/${page}`
+    })
+})
+
+let config = {
     entry: './app/assets/scripts/App.js',
-    output: {
+    plugins: pages,
+    module: {
+        rules: [
+            cssConfig
+        ]
+    }
+}
+
+if (currentTask == 'dev') {
+    cssConfig.use.unshift('style-loader')
+    config.output = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
-    },
-
-    // dev Server updates CSS & JS files in browser without full reload    
-    devServer: {
-    
+    }
+    config.devServer = {
+        
+        // dev Server updates CSS & JS files in browser without full reload 
+        
         //The following code is supposed to relaod the browser after html changes.
         //However, it does not.  (Online search: Webpack v5 dropped 'before')
              
@@ -38,14 +77,39 @@ module.exports = {
         //type 192.168.x.x:3000 into device browser to generate website.
         //Replace .x.x in 192.168.x.x with remainder of local IP address.
         //see Section 6.20 @ time=12:30
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]   // JavaScript loads css files
-            }
-        ]
     }
+    config.mode = 'development'
 }
+
+if (currentTask == 'build') {
+    config.module.rules.push({
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    })
+
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+    config.output = {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'docs')
+    }
+    config.mode = 'production'
+    config.optimization = {
+        splitChunks: {chunks: 'all'},
+        minimize: true,
+        minimizer: [`...`, new CssMinimizerPlugin()]
+    }
+    config.plugins.push(
+        new CleanWebpackPlugin(), 
+        new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+        new RunAfterCompile()
+        )
+}
+
+module.exports = config
